47238c3edab8d18a20e963704acc9e99111f8a65
Gohostieee <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>
2022-08-16 22:33:03 -0400

    initial commit!

---
+++ main.go
@@ -0,0 +1,79 @@
+// client.go
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var done chan interface{}
+var interrupt chan os.Signal
+var count int64 = 0
+
+func receiveHandler(connection *websocket.Conn) {
+	defer close(done)
+
+	for {
+		_, msg, err := connection.ReadMessage()
+		if err != nil {
+			log.Println("Error in receive:", err)
+			return
+		}
+		log.Printf("Received: something! %c, %d", msg[1], len(msg))
+	}
+}
+
+func main() {
+
+	done = make(chan interface{})    // Channel to indicate that the receiverHandler is done
+	interrupt = make(chan os.Signal) // Channel to listen for interrupt signal to terminate gracefully
+
+	signal.Notify(interrupt, os.Interrupt) // Notify the interrupt channel for SIGINT
+
+	socketUrl := "ws://localhost:8080" + "/socket"
+	conn, _, err := websocket.DefaultDialer.Dial(socketUrl, nil)
+	if err != nil {
+		log.Fatal("Error connecting to Websocket Server:", err)
+	}
+	defer conn.Close()
+	go receiveHandler(conn)
+	// Our main loop for the client
+	// We send our relevant packets here
+	for {
+		select {
+		case <-time.After(time.Millisecond * 100):
+			// Send an echo packet every second (or some other time measurement based out of a millisecond)
+
+			err := conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("getMap")))
+			if err != nil {
+				log.Println("Error during writing to websocket:", err)
+				return
+			}
+
+		case <-interrupt:
+			// We received a SIGINT (Ctrl + C). Terminate gracefully...
+			log.Println("Received SIGINT interrupt signal. Closing all pending connections")
+
+			// Close our websocket connection
+			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("Error during closing websocket:", err)
+				return
+			}
+
+			select {
+			case <-done:
+				log.Println("Receiver Channel Closed! Exiting....")
+			case <-time.After(time.Duration(100) * time.Second):
+				log.Println("Timeout in closing receiving channel. Exiting....")
+			}
+			return
+		}
+	}
+
+}
